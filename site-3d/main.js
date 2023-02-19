4d86bb9641440b2264993397f6e53160c6e1ed68
Josh Darling <squiddarling@gmail.com>
2023-02-18 19:14:23 -0500

    created camera, renderer, and camera movement on z

--- site-3d/main.js
+++ site-3d/main.js
@@ -2,3 +2,13 @@ import './style.css'
 import * as Three from 'three';
 
 
+const scene = new THREE.scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/innerHeight, 01, 1000);
+
+const renderer = new THREE.WebGLRenderList({
+  canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,innerHeight);
+camera.position.setZ(30);
