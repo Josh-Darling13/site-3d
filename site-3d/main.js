4cdd85bc04d9e5005629b85716efbcc3e95f9de5
Josh Darling <squiddarling@gmail.com>
2023-02-19 20:42:57 -0500

    finished mars, started on html and CSS

--- site-3d/main.js
+++ site-3d/main.js
@@ -15,27 +15,20 @@ renderer.setSize(window.innerWidth,innerHeight);
 camera.position.setZ(30);
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10,3,16,100 );
-const material = new THREE.MeshStandardMaterial({color: 0xff6347});
-const torus = new THREE.Mesh(geometry,material);
-
-scene.add(torus);
-
-const pointLight = new THREE.PointLight(0xffffff);
+const pointLight = new THREE.PointLight(0x9f0033);
 pointLight.position.set(50,50,50);
-
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar(){
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const geometry = new THREE.SphereGeometry(0.15, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0x99ccff})
   const star = new THREE.Mesh(geometry, material);
   const[x,y,z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100));
   star.position.set(x,y,z);
@@ -44,13 +37,39 @@ function addStar(){
 
 Array(300).fill().forEach(addStar);
 
+const backgrd = new THREE.TextureLoader().load('./img/MARSWINS copy.jpg');
+scene.background = backgrd;
+
+
+// const geometry = new THREE.TorusGeometry(10,3,16,100 );
+// const material = new THREE.MeshStandardMaterial({color: 0xff6347});
+// const torus = new THREE.Mesh(geometry,material);
+// scene.add(torus);
+
+const marsText = new THREE.TextureLoader().load('./img/HOME.jpg');
+const marsNormieText = new THREE.TextureLoader().load('./img/mars_normal.jpg');
+
+const mars = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map:marsText,
+    normalMap:marsNormieText,
+  }
+
+  )
+);
+scene.add(mars);
+
 function animate ()
 {
 requestAnimationFrame(animate);
 
-torus.rotation.x += 0.01;
-torus.rotation.y += 0.005;
-torus.rotation.z += 0.01;
+// torus.rotation.x += 0.01;
+// torus.rotation.y += 0.005;
+// torus.rotation.z += 0.01;
+
+
+mars.rotation.y += 0.01;
 
 controls.update();
 
