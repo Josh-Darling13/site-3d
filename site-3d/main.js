be86b49d2c028b31fbc9c83d8fa564da641f4473
Josh Darling <squiddarling@gmail.com>
2023-02-20 14:37:42 -0500

    added overlay html

--- site-3d/main.js
+++ site-3d/main.js
@@ -27,15 +27,15 @@ scene.add(pointLight, ambientLight);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar(){
-  const geometry = new THREE.SphereGeometry(0.15, 24, 24);
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({color: 0x99ccff})
   const star = new THREE.Mesh(geometry, material);
-  const[x,y,z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100));
+  const[x,y,z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(300));
   star.position.set(x,y,z);
   scene.add(star);
 }
 
-Array(300).fill().forEach(addStar);
+Array(200).fill().forEach(addStar);
 
 const backgrd = new THREE.TextureLoader().load('./img/MARSWINS copy.jpg');
 scene.background = backgrd;
@@ -50,15 +50,32 @@ const marsText = new THREE.TextureLoader().load('./img/HOME.jpg');
 const marsNormieText = new THREE.TextureLoader().load('./img/mars_normal.jpg');
 
 const mars = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.SphereGeometry(12, 128, 128), //(3, 32, 32),
   new THREE.MeshStandardMaterial({
     map:marsText,
     normalMap:marsNormieText,
   }
-
   )
 );
 scene.add(mars);
+
+mars.position.z = 30;
+mars.position.setX(-10);
+
+
+function moveCamera(){
+ const t = document.body.getBoundingClientRect().top;
+// mars.rotation.x += 0.05;
+// mars.rotation.y += 0.075;
+// mars.rotation.z += 0.05;
+mars.rotation.y += 0.01;// 
+
+camera.position.z = t * -0.001;
+camera.position.x = t * -0.03;
+camera.position.y = t * -0.03;
+}
+document.body.onscroll = moveCamera
+
 
 function animate ()
 {
